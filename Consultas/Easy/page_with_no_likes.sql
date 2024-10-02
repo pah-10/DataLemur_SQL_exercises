e5325200f5fb8d14cd2cafac290ffc2f54e51aa5
Paola Capita <61804889+pah-10@users.noreply.github.com>
2024-10-02 19:43:15 -0300

    Update page_with_no_likes.sql

--- Consultas/Easy/page_with_no_likes.sql
+++ Consultas/Easy/page_with_no_likes.sql
@@ -3,7 +3,6 @@
 
   Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
   Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs.
-
 */
 
 SELECT pages.page_id 
